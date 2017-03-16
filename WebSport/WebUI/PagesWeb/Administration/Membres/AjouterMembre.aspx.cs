f970b1b0363c0d7f27f5ced0bef1e796d235043f
Myredge <gaetan.pen49@gmail.com>
2017-03-16 09:34:48 +0100

    Ajouter des pages d'administration : - Gestion des courses - Gestion des membres

---
+++ WebSport/WebUI/PagesWeb/Administration/Membres/AjouterMembre.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebUI.PagesWeb.Administration.Membres
+{
+    public partial class AjouterMembre : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

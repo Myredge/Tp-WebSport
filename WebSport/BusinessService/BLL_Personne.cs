6bae0c513a9f58242e7968d9abf46e9e5e87dbad
Myredge <gaetan.pen49@gmail.com>
2017-03-17 16:50:31 +0100

    Ajout du 17/03/2017

---
+++ WebSport/BusinessService/BLL_Personne.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObject;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessService
+{
+    public class BLL_Personne
+    {
+        public static List<Personne> ListePersonne()
+        {
+            List<Personne> personnes = new List<Personne>();
+            DA_Personnes.PersonneDataTable dt = DAL_Personne.DonneToutes();
+
+            foreach (DataRow oRow in dt)
+            {
+                Personne pers = new Personne(Convert.ToInt32(oRow["ID"]), oRow["Nom"].ToString(), oRow["Prenom"].ToString());
+
+                personnes.Add(pers);
+            }
+
+            return personnes;
+        }
+    }
+}
---
+++ WebSport/WebUI/PagesWeb/Participant/Courses/ListeCoursesDisponible.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PagesWeb/Participant/MasterPageParticipant.Master" AutoEventWireup="true" CodeBehind="ListeCoursesDisponible.aspx.cs" Inherits="WebUI.PagesWeb.Participant.Courses.ListeCourseDisponible" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>

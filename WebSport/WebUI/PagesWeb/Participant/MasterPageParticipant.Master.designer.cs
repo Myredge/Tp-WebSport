c0b81fab6d1781f53ca9b781104dbdda06df6fee
HAMZI <hamziwasim@gmail.com>
2017-03-23 10:08:32 +0100

    page html ok

--- WebSport/WebUI/PagesWeb/Participant/MasterPageParticipant.Master.designer.cs
+++ WebSport/WebUI/PagesWeb/Participant/MasterPageParticipant.Master.designer.cs
@@ -22,6 +22,15 @@ namespace WebUI.PagesWeb {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// Contrôle form1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
         /// Contrôle MainContent.
         /// </summary>
         /// <remarks>
